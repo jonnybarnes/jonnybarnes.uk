476a2d1c5af25afe5173916bddb69ee3bff93b30
Jonny Barnes <jonny@jonnybarnes.uk>
2016-07-18 10:01:51 +0100

    Dump laravel-elixir, use gulp natively to compile sass, autoprefix declarations, and compress assets.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var zopfli = require('gulp-zopfli');
 var brotli = require('gulp-brotli');
+var zopfli = require('gulp-zopfli');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     return gulp.src('./resources/assets/sass/global.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({browsers: ['last 2 version']}))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./public/assets/css'));
 });
