a72d26269a56602f5fb68411b2f6b83709b4f69a
Jonny Barnes <jonny@jonnybarnes.uk>
2016-09-19 11:28:51 +0100

    Update typekit sri value

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -30,7 +30,7 @@
   <main>
 @yield('content')
   </main>
-  <script src="https://use.typekit.net/kmb3cdb.js" integrity="sha384-K/4E0NzJZXdpsxDKWbpP3NkSG+eA9slO7vv62+eOYgGPD142NqbSIvjcoVGvEh/r" crossorigin="anonymous"></script>
+  <script src="https://use.typekit.net/kmb3cdb.js" integrity="sha384-OB5f9FHlHSvyS7naCO3mhhaMFEPYwqwcyFjiY6HdkAq3MihmBkC2KW8QmYboUwS3" crossorigin="anonymous"></script>
   <script>try{Typekit.load({ async: true });}catch(e){}</script>
   @section('scripts')
   <!--scripts go here when needed-->
