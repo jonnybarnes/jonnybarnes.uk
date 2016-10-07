dd0a3228c1e1ff3688e2044300b1afccadfd15a6
Jonny Barnes <jonny@jonnybarnes.uk>
2016-10-07 12:16:33 +0100

    Use the correct URL format for places

--- app/Http/Controllers/MicropubController.php
+++ app/Http/Controllers/MicropubController.php
@@ -150,7 +150,7 @@ class MicropubController extends Controller
                 $distance = (count($matches[0]) == 3) ? 100 * $matches[0][2] : 1000;
                 $places = Place::near($matches[0][0], $matches[0][1], $distance);
                 foreach ($places as $place) {
-                    $place->uri = config('app.url') . '/place/' . $place->slug;
+                    $place->uri = config('app.url') . '/places/' . $place->slug;
                 }
 
                 return response()->json([
