b6eddec7efcd12a5942c4741ce5eea69cffc5c1c
Jonny Barnes <jonny@jonnybarnes.uk>
2017-02-23 15:11:05 +0000

    the script wasn’t outputting the curl response

--- travis/upload-failure-screenshots.sh
+++ travis/upload-failure-screenshots.sh
@@ -2,5 +2,5 @@
 
 for f in tests/Browser/screenshots/*.png
 do
-    curl -F "upload=@$f" -F "format=txt" http://uploads.im/api
+    curl -v -F "upload=@$f" -F "format=txt" http://uploads.im/api
 done
