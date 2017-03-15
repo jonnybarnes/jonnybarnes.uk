16015d3f4bb40a14ad1a521c006022b5f4c84431
Jonny Barnes <jonny@jonnybarnes.uk>
2017-03-15 19:16:49 +0000

    Allow string for when there are no syndication targets

--- resources/views/micropub/config.blade.php
+++ resources/views/micropub/config.blade.php
@@ -9,7 +9,7 @@ Micropub Config «
 <dl>
     <dt>Me (your url)</dt><dd>{{ $data['me'] }}</dd>
     <dt>Token</dt><dd>{{ $data['token'] }}</dd>
-    <dt>Syndication Targets</dt><dd><ul>@foreach ($data['syndication'] as $syn)<li>{{ $syn['name'] }} ({{ $syn['target'] }})</li>@endforeach</ul></dd>
+    <dt>Syndication Targets</dt><dd>@if(is_array($data['syndication']))<ul>@foreach ($data['syndication'] as $syn)<li>{{ $syn['name'] }} ({{ $syn['target'] }})</li>@endforeach</ul>@else{{ $data['syndication'] }}@endif</dd>
     <dt>Media Endpoint</dt><dd>{{ $data['media-endpoint'] }}</dd>
 </dl>
 <p><a href="{{ route('micropub-query-action') }}">Re-query</a> the endpoint.</p>
