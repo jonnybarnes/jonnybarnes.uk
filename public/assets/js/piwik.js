4f87dc309eada6f6ecd8c0ecb3a5e8ef96adb2f6
Jonny Barnes <jonny@jonnybarnes.uk>
2017-03-20 23:55:48 +0000

    Basic js uglification

--- public/assets/js/piwik.js
+++ public/assets/js/piwik.js
@@ -1,8 +1,1 @@
-// Piwik in its own js file to allow usage with a CSP policy
-
-var _paq = _paq || [];
-// tracker methods like "setCustomDimension" should be called before "trackPageView"
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-_paq.push(['setTrackerUrl', 'https://analytics.jmb.lv/piwik.php']);
-_paq.push(['setSiteId', '1']);
+var _paq=_paq||[];_paq.push(["trackPageView"]);_paq.push(["enableLinkTracking"]);_paq.push(["setTrackerUrl","https://analytics.jmb.lv/piwik.php"]);_paq.push(["setSiteId","1"]);
