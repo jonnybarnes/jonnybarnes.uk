997ade3c34272e6e034eb280fcc455b9df79fcca
Jonny Barnes <jonny@jonnybarnes.uk>
2017-03-24 15:50:17 +0000

    Return specific response

--- app/Http/Middleware/VerifyMicropubToken.php
+++ app/Http/Middleware/VerifyMicropubToken.php
@@ -16,7 +16,11 @@ class VerifyMicropubToken
     public function handle($request, Closure $next)
     {
         if ($request->bearerToken() === null) {
-            abort(401);
+            return response()->json([
+                'response' => 'error',
+                'error' => 'unauthorized',
+                'error_description' => 'No access token was provided in the request',
+            ], 401);
         }
 
         return $next($request);
