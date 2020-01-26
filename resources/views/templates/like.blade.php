1deb1a93aeea2c47993464c9c6c85c02b691dcaf
Jonny Barnes <jonny@jonnybarnes.uk>
2020-01-26 12:19:45 +0000

    For likes, add an internal link, e.g. /like/{id}

--- resources/views/templates/like.blade.php
+++ resources/views/templates/like.blade.php
@@ -1,19 +1,26 @@
 <div class="h-entry">
     <div class="h-cite u-like-of">
-        Liked <a class="u-url" href="{{ $like->url }}">a post</a>
+        <p>
+            Liked <a class="u-url" href="{{ $like->url }}">a post</a>
+        </p>
         @isset($like->author_name)
-            by <span class="p-author h-card">
+            <div>
+                by <span class="p-author h-card">
                 @isset($like->author_url)
                     <a class="u-url p-name" href="{{ $like->author_url }}">{{ $like->author_name }}</a>
                 @else
                     <span class="p-name">{{ $like->author_name }}</span>
                 @endisset
-            </span>
+                </span>
+            </div>
         @endisset
         @isset($like->content)
             <blockquote class="e-content">
                 {!! $like->content !!}
             </blockquote>
         @endisset
+        <div>
+            <a href="/likes/{{ $like->id }}"><time class="dt-published" datetime="{{ $like->updated_at->toISO8601String() }}" title="{{ $like->updated_at->toISO8601String() }}">{{ $like->updated_at->diffForHumans() }}</time></a>
+        </div>
     </div>
 </div>
