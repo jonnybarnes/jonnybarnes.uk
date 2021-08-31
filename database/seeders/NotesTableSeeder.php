d2123f6bfab291ad8137e3e93c8e3270ba8c482d
Jonny Barnes <jonny@jonnybarnes.uk>
2021-08-31 16:50:49 +0100

    Fix data for local page loading

--- database/seeders/NotesTableSeeder.php
+++ database/seeders/NotesTableSeeder.php
@@ -209,12 +209,6 @@ EOF;
             ->where('id', $noteWithLongUrl->id)
             ->update(['updated_at' => $now->toDateTimeString()]);
 
-        $noteWithLocation = Note::create([
-            'note' => 'Some location data',
-            'created_at' => $now->subDay(),
-            'location' => '1.23,4.56',
-        ]);
-
         Note::factory(10)->create();
     }
 }
