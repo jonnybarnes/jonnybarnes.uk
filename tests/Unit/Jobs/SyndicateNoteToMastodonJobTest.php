6f6abae25841bf63552022bd5aebeb44b80de647
Jonny Barnes <jonny@jonnybarnes.uk>
2022-11-06 10:16:18 +0000

    When posting to Mastodon send the original markdown

--- tests/Unit/Jobs/SyndicateNoteToMastodonJobTest.php
+++ tests/Unit/Jobs/SyndicateNoteToMastodonJobTest.php
@@ -8,6 +8,7 @@ use Faker\Factory;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -35,5 +36,34 @@ class SyndicateNoteToMastodonJobTest extends TestCase
         $this->assertDatabaseHas('notes', [
             'mastodon_url' => 'https://mastodon.example/@jonny/' . $randomNumber,
         ]);
+    }
+
+    /** @test */
+    public function weSyndicateTheOriginalMarkdown(): void
+    {
+        config(['bridgy.mastodon_token' => 'test']);
+        $faker = Factory::create();
+        $randomNumber = $faker->randomNumber();
+
+        $container = [];
+        $history = Middleware::history($container);
+        $mock = new MockHandler([
+            new Response(201, ['Location' => 'https://mastodon.example/@jonny/' . $randomNumber]),
+        ]);
+        $handler = HandlerStack::create($mock);
+        $handler->push($history);
+        $client = new Client(['handler' => $handler]);
+
+        $note = Note::factory()->create(['note' => 'This is a **test**']);
+        $job = new SyndicateNoteToMastodon($note);
+        $job->handle($client);
+
+        $this->assertDatabaseHas('notes', [
+            'mastodon_url' => 'https://mastodon.example/@jonny/' . $randomNumber,
+        ]);
+
+        $expectedRequestContent = '{"type":["h-entry"],"properties":{"content":["This is a **test**"]}}';
+
+        $this->assertEquals($expectedRequestContent, $container[0]['request']->getBody()->getContents());
     }
 }
