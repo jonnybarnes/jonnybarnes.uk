e1731d2ed37e49e75360963fa4e12da1df76829b
Jonny Barnes <jonny@jonnybarnes.uk>
2023-05-19 14:15:18 +0100

    Increase article title and URL column length in migration.
    - Increase character lengths for `titleurl` and `url` columns in `articles` table
    - Add migration file to alter column lengths in database

---
+++ database/migrations/2023_05_19_131031_alter_article_column_lengths.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('titleurl', 255)->change();
+            $table->string('url', 255)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            // Converting back to shorter lengths might cause issues
+        });
+    }
+};
