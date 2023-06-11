795c25c19ce1a6596d3d46b2cb49912bf5d5697c
Jonny Barnes <jonny@jonnybarnes.uk>
2023-06-11 13:05:14 +0100

    refactor: Remove unnecessary import in Note model

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -12,7 +12,6 @@ use GuzzleHttp\Client;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
